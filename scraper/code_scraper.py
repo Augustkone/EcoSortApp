81977f53b0db988f1819897b54f6a88fdb575363
Angelo <ranaivoangelo@gmail.com>
2026-07-09 14:43:03 +0000

    Ajout de commentaire, ce que fait le code

---
+++ scraper/code_scraper.py
@@ -0,0 +1,9 @@
+"""
+Il s'agit de ramener les infos suivantes pour les cinq premiers produit du résultat de recherche (mot clé) :
+- Nome du produit
+- Lien vers la fiche produit
+- Prix
+- Image du produit (lien)
+- Notation (sur 5)
+
+"""
