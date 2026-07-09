fa61db0e0fd59a80874a4aa9302b1d05cc8e9dbe
Angelo <ranaivoangelo@gmail.com>
2026-07-09 16:00:43 +0000

    Première version de scrap_jumia

--- scraper/code_scraper.py
+++ scraper/code_scraper.py
@@ -40,46 +40,27 @@ def scrap_jumia(keyword: str, max_results: int = 5) -> list[dict]:
         if len(resultats) >= max_results:
             break
         
-        
+        lien = produit.get("href")
+        lien_complet = "https://www.jumia.ci" + lien
 
+        nom_tag = produit.find("h3", class_="name")
+        nom = nom_tag.text if nom_tag else "Nom inconnu"
 
-with open("test.html", "r", encoding="utf-8") as f:
-    html = f.read()
+        prix_tag = produit.find("div", class_="prc")
+        prix = prix_tag.text if prix_tag else "Prix inconnu"
 
-soup = BeautifulSoup(html, "lxml")
+        img_tag = produit.find("img")
+        image_url = img_tag.get("data-src") or img_tag.get("src") if img_tag else None
 
-# find_all au lieu de find : on récupère TOUS les produits, pas juste le premier
-produits = soup.find_all("a", class_="core")
+        stars_div = produit.find("div", class_="stars _s")
+        note = stars_div.text.strip() if stars_div else "Pas de note"
 
-print(f"Nombre de produits trouvés : {len(produits)}")
+        resultats.append({
+            "nom": nom,
+            "prix": prix,
+            "image": image_url,
+            "note": note,
+            "lien": lien_complet
+        })
+    return resultats
 
-resultats = []
-
-for produit in produits:
-    lien = produit.get("href")
-    lien_complet = "https://www.jumia.ci" + lien
-
-    nom_tag = produit.find("h3", class_="name")
-    nom = nom_tag.text if nom_tag else "Nom inconnu"
-
-    prix_tag = produit.find("div", class_="prc")
-    prix = prix_tag.text if prix_tag else "Prix inconnu"
-
-    img_tag = produit.find("img")
-    image_url = img_tag.get("data-src") or img_tag.get("src") if img_tag else None
-
-    stars_div = produit.find("div", class_="stars _s")
-    note = stars_div.text.strip() if stars_div else "Pas de note"
-
-    resultats.append({
-        "nom": nom,
-        "prix": prix,
-        "image": image_url,
-        "note": note,
-        "lien": lien_complet
-    })
-
-# On affiche les 3 premiers pour vérifier
-for r in resultats[:3]:
-    print(r)
-    print("---")
